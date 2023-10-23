3e041c61733096c1ad5cc6eeb01248c2a7e07c76
serega_sneg93@mail.ru <serega_sneg93@mail.ru>
2023-10-23 17:09:06 +0700

    Change Of part 3 End

--- Program.cs
+++ Program.cs
@@ -12,6 +12,7 @@ builder.Services.AddDbContext<DataContext>(options =>
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -25,6 +26,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseRouting();
+app.UseCors(policy => policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 app.UseAuthorization();
 
 app.MapControllers();
