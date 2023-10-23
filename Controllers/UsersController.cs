ea83ddbdac137a6fc3a764b2ee783a4238f30804
serega_sneg93@mail.ru <serega_sneg93@mail.ru>
2023-10-23 14:48:02 +0700

    Update

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,32 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public UsersController(DataContext dataContext)
+        {
+            _dataContext = dataContext;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _dataContext.Users.ToListAsync();
+        }
+
+        // api/users/3
+        [HttpGet("{idUser}")]
+        public async Task<ActionResult<AppUser>> GetUser(int idUser)
+        {
+            return await _dataContext.Users.FindAsync(idUser);
+        }
+    }
+}
